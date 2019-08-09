023b2a489db07598a19bf3a059e9cb3b41a6e04e
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2019-08-09 21:18:15 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
-def login():    
-    user = raw_input("Username: ")
-    passw = raw_input("Password: ")
-    f = open("users.txt", "r")
-    for line in f.readlines():
-        us, pw = line.strip().split("|")
-        if (user in us) and (passw in pw):
-            print "Login successful!"
-            return True
-    print "Wrong username/password"
-    return False
-
-def menu():
-    #here's a menu that the user can access if he logged in.
-
-def main():
-    login()
-    log = login()
-    if log == True:
-         menu()
+user = raw_input(‘Username: ‘)
+import getpass
+sandi = getpass.getpass()
+if sandi == ‘123’ and user == ‘abc’:
+print ‘Anda Telah Login’
+else:
+print ‘Username atau Password Anda Salah’
