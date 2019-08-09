d27fd7ac8009531dfe70312be6927daeec4095d0
Bintang73 <42708989+Bintang73@users.noreply.github.com>
2019-08-09 22:31:42 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,68 @@
-user = raw_input(‘Username: ‘)
-import getpass
-sandi = getpass.getpass()
-if sandi == ‘123’ and user == ‘abc’:
-print ‘Anda Telah Login’
-else:
-print ‘Username atau Password Anda Salah’
+#tools by St4rs
+#Jangan edit selain username+password
+#recode gk bikin lo jadi mastah
+
+
+from getpass import getpass
+import os
+import time
+
+def menu():
+      while True:
+           print("")
+           os.system("clear")
+           os.system("figlet -f slant '   wellcome' | lolcat")
+           print('\033[1;96m ============================================================')
+           print(' ||  +--------------------------------------------------+  ||')
+           print(' ||  | Creator   : Bintang Nur Pradana                             |  ||')
+           print(' ||  +--------------------------------------------------+  ||')
+           print(' ||  | Youtube   : https://youtube.com/xnxx             |  ||')
+           print(' ||  | github    : https://github.com/bintangnurpradana |  ||')
+           print(' ||  | WhatsApp  : 081329896085                         |  ||')
+           print(' ||  | Team      : Garuda Terisakti 72                  |  ||')
+           print(' ||  +--------------------------------------------------+  ||')
+           print(' ============================================================')
+           print("")
+           try:
+                x = str(input('\033[1;92m [?] Username \033[1;93m: '))
+                print("")
+                e = getpass('\033[1;92m [?] Password \033[1;93m: ')
+                print ("")
+#silahkan ganti username+passwordnya gan
+                if x=="admin" and e=="admin":
+#jangan edit yg lain kalo gak mau eror
+                   print('Login Sukses Mohon Tunggu Sebentar...')
+                   time.sleep(1)
+                   os.system('clear')
+                   print('')
+                   os.system('figlet ' + x + ' | lolcat')
+                   print('\033[1;92m ────────────────────────────────|by Moreno77 ')
+                   print("")
+                   break
+                else:
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     [!] Username/Password Salah")
+                      time.sleep(2)
+                      print("")
+           except Exception:
+                      
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     [!] Username/Password Salah")
+                      time.sleep(2)
+           except KeyboardInterrupt:
+                      print("")
+                      os.system('killall -9 com.termux/home/example2')
+                      print("")
+                      print("")
+                      print("")
+                      print("")
+                      print("\033[1;91m     [!] Username/Password Salah")
+                      time.sleep(2)
+menu()
